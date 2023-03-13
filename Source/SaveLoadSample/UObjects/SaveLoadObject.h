09df72ffe26f6966aa2e905975920af46d34ea04
Ivan <42315133+Antonbreakble@users.noreply.github.com>
2023-03-13 10:47:12 +0300

    SaveLoadObject

---
+++ Source/SaveLoadSample/UObjects/SaveLoadObject.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SaveLoadSample/Structs/FSaveDataRecord.h"
+#include "UObject/Object.h"
+#include "SaveLoadObject.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable)
+class SAVELOADSAMPLE_API USaveLoadObject : public UObject
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable, Category=SaveLoad)
+	FSaveDataRecord GetSaveDataForObject(UObject* SavableObject) const;
+
+	UFUNCTION(BlueprintCallable, Category=SaveLoad)
+	void LoadObjectFormSaveData(UObject* SavableObject, FSaveDataRecord Record);
+};
